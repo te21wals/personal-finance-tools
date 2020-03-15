7186801ab5614527dd2775ae273cd4e9321e77c0
Thomas Walsh <twalsh@commercehub.com>
2020-03-14 21:47:35 -0400

    rename module, and clean up commented out code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,5 @@
-const {
-    getAllTransactions
-} = require('./parse-export/get-all-export-transactions');
+const { getAllTransactions } = require('./parse-export/get-all-transactions');
 
-// async function reduce(asyncIter, reduceFunction, init = []) {
-//     let res = init;
-//     for await (const x of asyncIter) {
-//         res = reduceFunction(res, x);
-//     }
-//     return res;
-// }
-
-// const toArray = async iter => await reduce(iter, (a, x) => [...a, ...x]);
-
-// (async () => {
-//     const transactions = [];
-//     for await (const transactionExport of getExports('./exports')) {
-//         const transactionsForExport = await parseExport(transactionExport);
-//         transactions.concat(transactionsForExport);
-//     }
-//     console.log(transactions);
-// })();
 (async () => {
     const transactions = await getAllTransactions('./exports');
     console.log(transactions);
